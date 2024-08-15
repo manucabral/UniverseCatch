905ab2dea9d9a8b022e7a899973ab5e80d9cfbff
Leo Araya <larayavrs@gmail.com>
2024-08-15 17:38:12 -0400

    feat: add initial project metadata

---
+++ source/__init__.py
@@ -0,0 +1,7 @@
+"""A strategic adaptation of Go Fish, featuring cards with planets, stars, black holes, and galaxies"""
+
+__author__ = "Universe Catch, Inc."
+__copyright__ = "Copyright 2024 Universe Catch"
+__developers__ = ["manucabral", "larayavrs"]
+__version__ = "0.1.0"
+__license__ = "GNU GPL v3.0"
