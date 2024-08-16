48319e269bbc8d1f0750e6feccd8a0d9a59d0abd
Manu <mandrescabral@gmail.com>
2024-08-16 19:30:45 -0300

    refactor: update __init__.py to use GameConfig for version and remove unused imports

--- source/__init__.py
+++ source/__init__.py
@@ -1,7 +1,13 @@
 """A strategic adaptation of Go Fish, featuring cards with planets, stars, black holes, and galaxies"""
 
+from .controller import Controller
+from .constants import GameConfig
+
 __author__ = "Universe Catch, Inc."
 __copyright__ = "Copyright 2024 Universe Catch"
 __developers__ = ["manucabral", "larayavrs"]
-__version__ = "0.1.0"
+__version__ = GameConfig.VERSION
 __license__ = "GNU GPL v3.0"
+__all__ = [
+    "Controller",
+]
