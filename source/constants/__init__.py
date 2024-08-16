95b25815fe406d193f3b74d0b36f160586fadbfd
Manu <mandrescabral@gmail.com>
2024-08-16 19:30:57 -0300

    feat: add constants module for storing project constants

---
+++ source/constants/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module is used to store all the constants used in the project.
+"""
+
+from .colors import Colors
+from .game import GameConfig
+from .display import DisplayConfig
+from .network import NetworkConfig
+from .resource import ResourceConfig
+
+__all__ = ["Colors", "DisplayConfig", "NetworkConfig", "ResourceConfig", "GameConfig"]
