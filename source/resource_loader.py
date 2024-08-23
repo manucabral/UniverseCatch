e1753906edbbb03d361b0aa26df1f1c6e37c08b6
Manu <mandrescabral@gmail.com>
2024-08-23 18:35:14 -0300

    feat(resources): improve ResourceLoader to load all image planets

--- source/resource_loader.py
+++ source/resource_loader.py
@@ -5,6 +5,8 @@ Resources are images, sounds, and other files that are used in the game.
 
 import os
 import pygame as pyg
+from .logger import get_logger
+from .constants import ResourceConfig
 
 
 class ResourceLoader:
@@ -13,20 +15,37 @@ class ResourceLoader:
     """
 
     def __init__(self, resource_dir: str = "resources"):
+        self.logger = get_logger(self.__class__.__name__)
         if not os.path.isdir(resource_dir):
-            raise FileNotFoundError(f"Resource directory '{resource_dir}' not found.")
-        self.resource_dir = resource_dir
+            msg = f"Directory '{resource_dir}' not found."
+            self.logger.error(msg)
+            raise FileNotFoundError(msg)
+        self.dir = resource_dir
         self.fonts = {}
         self.images = {}
         self.sounds = {}
+        self.planets: dict = {}
 
     def load_all_fonts(self) -> None:
         pass
 
     def load_all_images(self) -> None:
         self.images["screen_loading"] = pyg.image.load(
-            os.path.join(self.resource_dir, "screen_loading.png")
+            os.path.join(self.dir, "screen_loading.png")
         )
+
+        """
+        Load all image planets
+        """
+        for item, item_path in ResourceConfig.PLANETS.items():
+            try:
+                path = os.path.join(ResourceConfig.PLANETS_DIR, item_path)
+                self.planets[item] = pyg.image.load(path)
+            except Exception as exc:
+                self.logger.error(
+                    f"Error loading planetId {item} from {path}, not found."
+                )
+                self.logger.error(exc, console=False)
 
     def load_all_sounds(self) -> None:
         pass
