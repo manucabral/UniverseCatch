48601e4f838e0f671a8df207055fdffbf76d7ca9
Manu <mandrescabral@gmail.com>
2024-08-23 19:18:46 -0300

    feat: add debug logging to MainMenuScene handle_event method

--- source/scenes/main_menu.py
+++ source/scenes/main_menu.py
@@ -19,7 +19,7 @@ class MainMenuScene(Scene):
         """
         Create a new intro scene.
         """
-        super().__init__("main_menu", controller)
+        super().__init__("main_menu", controller, debug=controller.debug)
         self.background: pyg.Surface = None
         self.buttons: list[Button] = []
 
@@ -27,6 +27,7 @@ class MainMenuScene(Scene):
         """
         Called when the scene is entered.
         """
+        self.log("Entering scene.")
         lang = self.controller.lang
         menu = self.controller.localizations.get_key("menu", lang)
         self.buttons.append(
@@ -37,14 +38,17 @@ class MainMenuScene(Scene):
                 font=pyg.font.Font(None, 24),
                 font_size=24,
                 font_color=Colors.WHITE,
-                action=lambda: self.controller.change_scene("intro"),
+                action=lambda: self.controller.change_scene("asdasd"),
                 position=(100, 100),
                 size=(100, 50),
                 color=Colors.BLUE,
+                debug=self.debug,
             )
         )
 
-    def on_exit(self): ...
+    def on_exit(self):
+        self.log("Exiting scene.")
+        self.buttons.clear()
 
     def update(self, screen: pyg.Surface, delta_time: float) -> None:
         screen.fill(Colors.BLACK)
@@ -53,4 +57,6 @@ class MainMenuScene(Scene):
 
     def render(self): ...
 
-    def handle_event(self, event: pyg.event.Event) -> None: ...
+    def handle_event(self, event: pyg.event.Event) -> None:
+        for button in self.buttons:
+            button.handle_event(event)
