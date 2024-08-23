10289bfa77a3dfd157b157aa5c6c5ae0904af526
Manu <mandrescabral@gmail.com>
2024-08-23 18:36:07 -0300

    feat(resources): add planets

--- source/constants/resource.py
+++ source/constants/resource.py
@@ -9,3 +9,18 @@ class ResourceConfig:
     """
 
     RESOURCE_DIR: str = "resources"
+    LOCALIZATIONS_DIR: str = RESOURCE_DIR + "/localizations"
+    PLANETS_DIR: str = RESOURCE_DIR + "/planets"
+
+    PLANETS: dict = {
+        0: "mercury.png",
+        1: "venus.png",
+        2: "earth.png",
+        3: "mars.png",
+        4: "jupiter.png",
+        5: "saturn.png",
+        6: "uranus.png",
+        7: "neptune.png",
+        8: "pluto.png",
+        9: "bellerophonn.png",
+    }
