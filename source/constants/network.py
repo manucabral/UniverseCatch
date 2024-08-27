82c0170053dd5d8bf124bf0999e7eb2e19a6105f
Manu <mandrescabral@gmail.com>
2024-08-27 00:45:39 -0300

    feat: update network constants for improved performance

--- source/constants/network.py
+++ source/constants/network.py
@@ -10,3 +10,7 @@ class NetworkConfig:
 
     HOST: str = "localhost"
     PORT: int = 5642
+    MAX_CONNECTIONS: int = 4
+    BUFFER_SIZE: int = 1024
+    TIMEOUT: int = 5
+    RECV_SIZE: int = 1024
